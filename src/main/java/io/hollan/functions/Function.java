47ebf99574e864baf1c655c5e07cf71c86419300
Jonathan Giles <jonathan@jonathangiles.net>
2018-09-21 12:12:25 +1200

    Update to the latest APIs for functions and computer vision.

--- src/main/java/io/hollan/functions/Function.java
+++ src/main/java/io/hollan/functions/Function.java
@@ -1,39 +1,40 @@
 package io.hollan.functions;
 
-import com.microsoft.azure.serverless.functions.annotation.*;
-import com.microsoft.azure.serverless.functions.*;
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.ImageAnalysis;
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.ImageTag;
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.VisualFeatureTypes;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
 
 import org.json.*;
 
-import com.microsoft.azure.cognitiveservices.computervision.*;
-import com.microsoft.azure.cognitiveservices.computervision.implementation.ComputerVisionAPIImpl;
-import com.microsoft.azure.cognitiveservices.computervision.implementation.ImageAnalysisInner;
-import com.microsoft.rest.credentials.ServiceClientCredentials;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.microsoft.azure.cognitiveservices.vision.computervision.implementation.ComputerVisionClientImpl;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Azure Functions with HTTP Trigger.
- */
 public class Function {
+
+    public static final String apiKey = System.getenv("CognitiveServicesApiKey");
+    public static final String endpoint = System.getenv("CognitiveServicesEndpoint");
 
     @FunctionName("analyze")
     public static void analyze(
             @BlobTrigger(path = "images", name = "image", connection = "AzureWebJobsStorage", dataType = "binary") byte[] image,
-            @DocumentDBOutput(name = "outputDocument", databaseName = "images", collectionName = "analysis", connection = "CosmosDbConnection") OutputBinding<String> outputDocument,
+            @CosmosDBOutput(name = "outputDocument", databaseName = "images", collectionName = "analysis", connectionStringSetting = "CosmosDbConnection") OutputBinding<String> outputDocument,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Create a client.
-        ComputerVisionAPIImpl client = Function.getClient(apiKey);
-        client.withAzureRegion(AzureRegions.WESTUS2);
-        List<VisualFeatureTypes> visualFeatureTypes = new ArrayList<VisualFeatureTypes>();
+        ComputerVisionClientImpl client = new ComputerVisionClientImpl(
+            endpoint,
+            builder -> builder.addNetworkInterceptor(chain -> chain.proceed(chain.request().newBuilder()
+                    .addHeader("Ocp-Apim-Subscription-Key", apiKey)
+                    .build())
+            )
+        );
+        client.withEndpoint(endpoint);
+        List<VisualFeatureTypes> visualFeatureTypes = new ArrayList<>();
         visualFeatureTypes.add(VisualFeatureTypes.DESCRIPTION);
         visualFeatureTypes.add(VisualFeatureTypes.CATEGORIES);
         visualFeatureTypes.add(VisualFeatureTypes.COLOR);
@@ -41,26 +42,22 @@ public class Function {
         visualFeatureTypes.add(VisualFeatureTypes.IMAGE_TYPE);
         visualFeatureTypes.add(VisualFeatureTypes.TAGS);
 
-        ImageAnalysisInner result = client.analyzeImageInStream(
-                image,
-                visualFeatureTypes,
-                null,
-                null);
+        ImageAnalysis result = client.computerVision().analyzeImageInStream()
+                .withImage(image)
+                .withVisualFeatures(visualFeatureTypes)
+                .execute();
 
         JSONObject document = new JSONObject();
         document.put("id", java.util.UUID.randomUUID());
 
-        if(result.description() != null &&
-                result.description().captions() != null) {
+        if(result.description() != null && result.description().captions() != null) {
             context.getLogger().info("The image can be described as: %s\n" + result.description().captions().get(0).text());
             document.put("description", result.description().captions().get(0).text());
         }
 
 
-        for(ImageTag tag : result.tags())
-        {
-            context.getLogger().info(
-                    String.format("%s\t\t%s", tag.name(), tag.confidence()));
+        for(ImageTag tag : result.tags()) {
+            context.getLogger().info(String.format("%s\t\t%s", tag.name(), tag.confidence()));
             document.append("tags", new JSONObject(String.format("{\"name\": \"%s\", \"confidence\": \"%s\" }", tag.name(), tag.confidence())));
         }
 
@@ -73,30 +70,4 @@ public class Function {
 
         outputDocument.setValue(document.toString());
     }
-
-
-    public static ComputerVisionAPIImpl getClient(final String subscriptionKey) {
-        return new ComputerVisionAPIImpl(
-                "https://westus2.api.cognitive.microsoft.com/vision/v1.0/",
-                new ServiceClientCredentials() {
-                    @Override
-                    public void applyCredentialsFilter(OkHttpClient.Builder builder) {
-                        builder.addNetworkInterceptor(
-                                new Interceptor() {
-                                    @Override
-                                    public Response intercept(Interceptor.Chain chain) throws IOException {
-                                        Request request = null;
-                                        Request original = chain.request();
-                                        // Request customization: add request headers
-                                        Request.Builder requestBuilder = original.newBuilder()
-                                                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                                        request = requestBuilder.build();
-                                        return chain.proceed(request);
-                                    }
-                                });
-                    }
-                });
-    }
-
-    public static String apiKey = System.getenv("CognitiveServicesApiKey");
 }
